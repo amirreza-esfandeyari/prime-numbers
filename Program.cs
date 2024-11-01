547fae59d9866f1b2c80ffb47e71cf8227903632
amirreza-esfandeyari <amirrezaesfandeyari@gmail.com>
2024-11-01 18:56:10 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Getting input from the user
+        Console.Write(" Enter the first number (m): ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write(" Enter the second number (n) : ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("اعداد آینه‌ای بین " + m + " و " + n + " عبارتند از:");
+
+        // Check any number between m و n
+        for (int number = m; number <= n; number++)
+        {
+            // Convert number to string
+            string strNumber = number.ToString();
+            // Reverse the string
+            char[] charArray = strNumber.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedNumber = new string(charArray);
+
+            // Check if the number is a mirror
+            if (strNumber == reversedNumber)
+            {
+                Console.Write(number + " "); // Mirror number printing
+            }
+        }
+    }
+}
